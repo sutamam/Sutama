d26e1d5c29f3af513e15e0f2e81eb572a45de85f
Sutama Mitra <sutama.mitra@outlook.com>
2020-06-20 12:56:34 -0400

    Initial Draft

---
+++ InClassCS3/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using InClassCS3.Models;
+
+
+namespace InClassCS3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult InputNameState(string name,string state)
+        {
+
+            var inp = new input() { name = name, state = state };
+
+            ViewBag.explaination = "Welcome "+ inp.name+" to the state of "+ inp.state;
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ InClassCS3/Models/input.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InClassCS3.Models
+{
+    public class input
+    {
+        public string name { get; set; }
+        public string state { get; set; }
+    }
+}
